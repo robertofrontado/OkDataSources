e2abacb8116da7c7a179e92c78ace8007889752d
Roberto Frontado <robertofrontado@gmail.com>
2018-11-09 10:16:59 +0100

    Updated Example

--- OkDataSourcesExample/Pods/RxSwift/RxSwift/Disposable.swift
+++ OkDataSourcesExample/Pods/RxSwift/RxSwift/Disposable.swift
@@ -1,14 +1,12 @@
 //
 //  Disposable.swift
-//  Rx
+//  RxSwift
 //
 //  Created by Krunoslav Zaher on 2/8/15.
 //  Copyright © 2015 Krunoslav Zaher. All rights reserved.
 //
 
-import Foundation
-
-/// Respresents a disposable resource.
+/// Represents a disposable resource.
 public protocol Disposable {
     /// Dispose resource.
     func dispose()
